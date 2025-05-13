b90f5fd065f70b8d983c12391f3ede0f7043c0b8
NicolasGuerrero <nicoguerrero0800@gmail.com>
2025-05-13 17:33:28 -0600

    feat: creacion auth usuarios

---
+++ src/controllers/auth.controller.js
@@ -0,0 +1,63 @@
+import User from '../models/user.model.js';
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+import {JWT_SECRET_KEY} from '../config/config.js';
+
+export const registerUser = async (req, res) => {
+    const {email, password} = req.body;
+    try{
+        //validator
+        const existUser = await User.findOne({email});
+        if(existUser)return res.status(401).json({message:"email already exists"})
+
+        //hash password
+        const hashPassword = await bcrypt.hash(password, 10);
+        const newUser = new User(
+            {
+                email,
+                password:hashPassword
+            });
+        //add new user to db
+        await newUser.save();
+        res.status(201).json({
+            message:"user created successfully",
+            user:{
+                id:newUser._id,
+                email:newUser.email,
+            }
+        })
+    }catch (error) {
+        return res.status(500).json({message:error.message})
+    }
+}
+
+export const loginUser = async (req, res) => {
+    const {email, password} = req.body
+
+    try{
+        const user = await User.findOne({email})
+        if (!user) return res.status(401).json({message:"Email not found"})
+
+        const match = await bcrypt.compare(password, user.password)
+        if(!match)return res.status(401).json({message:"Password is incorrect"})
+
+        const token = jwt.sign(
+            {id:user._id, name:user.username},
+            JWT_SECRET_KEY,
+            {expiresIn:"1h"}
+        )
+        res.status(200).json({
+            message:"Login successful",
+            token,
+            user: {
+                id: user._id,
+                email: user.email,
+                username: user.username,
+                role: user.role
+            }
+        })
+
+    }catch (error) {
+        return res.status(500).json({message:error.message})
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 import express from 'express';
 import { PORT } from './config/config.js';
 import connectDB from './config/db.js';
+import authRouter from './routes/auth.routes.js';
 
 const app = express()
 
+// middleware
+app.use(express.json())
+app.use('/api/auth', authRouter)
 
 
 const starServer = async () => {

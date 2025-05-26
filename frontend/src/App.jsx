5ab4073761e5d154d235a08c34d43c2813a59d46
NicoGuerrero11 <nicoguerrero0800@gmail.com>
2025-05-26 12:59:35 -0600

    feat: creamos cart

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,18 +2,21 @@ import { Routes, Route } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 import Products from './pages/Products'
-import Home from './pages/Home.jsx'
+import Cart from './pages/Cart.jsx'
 import MainLayout from './layouts/MainLayout'
 
 function App() {
     return (
         <Routes>
+            {/* Main Layout */}
             <Route path="/" element={<MainLayout />}>
-                <Route index element={<Home />} />
+                {/* Nested Routes */}
+                <Route index element={<Products />} />
                 <Route path="/products" element={<Products />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/cart" element={<Cart />} />
+                <Route path="/register" element={<Register />} />
             </Route>
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
         </Routes>
     );
 }

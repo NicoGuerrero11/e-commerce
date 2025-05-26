0cb77ca8afafcb270a72ac6d0eb9c5c3297bf265
NicoGuerrero11 <nicoguerrero0800@gmail.com>
2025-05-26 13:00:19 -0600

    feat: se creo un navbar para todas las paginas

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,20 +1,52 @@
-import { Outlet, Link } from 'react-router-dom'
-import { AppBar, Toolbar, Typography, Button } from '@mui/material'
+import { Outlet, Link, useLocation } from 'react-router-dom'
+import { AppBar, Toolbar, Typography, Button, Box } from '@mui/material'
 
-const MainLayout = () => (
-    <>
-        <AppBar position="static">
-            <Toolbar>
-                <Typography variant="h6"  sx={{ flexGrow: 1 }}>
-                    E-Commerce
-                </Typography>
-                <Button color="inherit" component={Link} to={"/"}>Home</Button>
-                <Button color="inherit" component={Link} to={"/Products"}>Products</Button>
-                <Button color="inherit" component={Link} to={"/Login"}>Login</Button>
-            </Toolbar>
-            <Outlet />
-        </AppBar>
-    </>
-);
+const MainLayout = () => {
+
+    const location = useLocation();
+
+    const pages = [
+        { name: 'Products', path: '/products' },
+        { name: 'Cart', path: '/cart' },
+        { name: 'Login', path: '/login' }
+    ];
+
+    const visiblePages = pages.filter((page) => {
+        if (page.path === '/') return location.pathname !== '/';
+        return location.pathname !== page.path;
+    });
+
+    return(
+
+        <>
+            <AppBar position="static">
+                <Toolbar sx={{px: 3, py:2}}>
+                        {/* <img src="/logo.png" alt="Logo" style={{ height: 40 }} /> */}
+                        <Typography variant="h4"  sx={{ flexGrow: 1 }}>
+                            E-Commerce
+                        </Typography>
+                        {visiblePages.map((page) => (
+                            <Button
+                                key={page.path}
+                                color="inherit"
+                                component={Link}
+                                to={page.path}
+                            >
+                                {page.name}
+                            </Button>
+                        ))}
+
+                </Toolbar>
+            </AppBar>
+            <main>
+                <Box sx={{ padding: 2 }}>
+                    <Outlet />
+                </Box>
+                
+            </main>
+        </>
+    )
+
+};
 
 export default MainLayout;

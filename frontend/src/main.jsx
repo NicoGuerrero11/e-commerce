f5abaf6d4b1fda211740eb3dcc99e9f371d01004
NicoGuerrero11 <nicoguerrero0800@gmail.com>
2025-05-26 12:59:47 -0600

    usamos el nuevo theme

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,12 +1,23 @@
-import React, {StrictMode} from 'react'
+import {StrictMode} from 'react'
 import ReactDOM from 'react-dom/client';
 import {BrowserRouter} from 'react-router-dom';
+import {ThemeProvider} from '@mui/material/styles';
+import theme from './theme/theme.js';
+
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
+
 import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <StrictMode>
-        <BrowserRouter>
-            <App />
-        </BrowserRouter>
+        <ThemeProvider theme={theme}>
+            <BrowserRouter>
+                <App />
+            </BrowserRouter>
+        </ThemeProvider>
     </StrictMode>,
 )
